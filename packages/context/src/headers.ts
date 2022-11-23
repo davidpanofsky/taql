8721dd10cb68d154ccbf49da617d0132fd352cec
jacobkatz <jacobkatz@tripadvisor.com>
2022-11-23 17:37:48 +0000

    DM-446 - initial header forwarding allowlist
    Build an allowlist of headers taql will forward upstream. This initial
    allowlist is mostly derived from the set of headers tripadvisor-web will
    send legacy graphql, plus a few app-proxy will set. We may later find
    out that we need more, but this is a good start.

--- packages/context/src/headers.ts
+++ packages/context/src/headers.ts
@@ -2,30 +2,60 @@ import { Headers as FetchHeaders } from 'node-fetch';
 import { Plugin } from '@envelop/core';
 import { YogaInitialContext } from 'graphql-yoga';
 
+// Note: we never want to forward x-tripadvisor-locale, x-tripadvisor-currency,
+// or the like. If the response depends on locale, it must be included in the
+// query parameters, end of story.
+
+export enum ForwardHeader {
+  'x-guid',
+  'x-request-id',
+  'x-unique-id',
+  'x-ta-unique-dec',
+  'x-ua',
+  'user-agent',
+  'cookie',
+  'x-service-overrides',
+  'x-txip',
+  'authorization',
+
+  // testing & debug headers
+  'lt',
+  'x-loadtest',
+  'x-swedwig-feed-viewer',
+
+  // tracing headers
+  'x-b3-traceid',
+  'x-b3-spanid',
+  'x-b3-parentspanid',
+  'x-b3-flags',
+  'x-b3-sampled',
+  'b3',
+}
+
+const FORWARD_HEADERS: ReadonlySet<keyof typeof ForwardHeader> = new Set(
+  <(keyof typeof ForwardHeader)[]>Object.keys(ForwardHeader)
+);
+
 export const copyHeaders = (
   headers: Headers | FetchHeaders | undefined,
-  filterPredicate?: (val: string, key: string) => boolean
+  filterPredicate?: (args: { val: string; key: string }) => boolean
 ): FetchHeaders => {
   const copy = new FetchHeaders();
   headers?.forEach((val, key) => {
-    if (filterPredicate == undefined || filterPredicate(val, key)) {
+    if (filterPredicate == undefined || filterPredicate({ val, key })) {
       copy.append(key, val);
     }
   });
   return copy;
 };
 
-// extremely lazy header forwarding. all our custom
-// headers start with x-, and I guess we should do cookies too.
-// Cookie forwarding ought to be a subject of debate, though.
-// Cut everything else for now.
-// TODO develop allow-list list of headers to forward
+FORWARD_HEADERS;
+
 const deriveHeaders = (
   context: YogaInitialContext & HeadersContext
 ): FetchHeaders =>
-  copyHeaders(
-    context.request.headers,
-    (val, key) => key.startsWith('x-') || key === 'cookie'
+  copyHeaders(context.request.headers, ({ key }) =>
+    FORWARD_HEADERS.has(<keyof typeof ForwardHeader>(<unknown>key))
   );
 
 export type HeadersContext = {
