808ea753881593821db1312770a6255fa2a75e3c
Scott Mitchell <smitchell@tripadvisor.com>
2023-01-09 06:54:11 -0500

    move from postgresql-client to pg, pre-test passing promise as parsed document

--- packages/prereg/src/index.ts
+++ packages/prereg/src/index.ts
@@ -1,5 +1,31 @@
 import { Plugin } from '@envelop/core';
+import { Pool } from 'pg';
 import { inspect } from 'util';
+
+// One single pool at the top level; it seems overly complicated to try to keep this inside the plugin object,
+// especially considering there's no lifecycle method for plugin destruction, meaning we arent' really going to
+// do anything smart with destroying the pool when the plugin goes away.
+//
+// Another approach would be to change the exported plugin to be a () => Plugin and initialize
+// the pool during the call.  That would be sweet if we were passed some params at that time.
+// TODO(smitchell): Climb to the top of the stack and see where this is initialized and if we have access to make
+// configuration passable
+const defaultDbUri = 'postgres://graphql_operations_ros@graphql-operations-ros.db.var.ml.tripadvisor.com'
+const connectionString = process.env['PREREGISTERED_QUERY_DB_URI'] || defaultDbUri;
+const db = new Pool({
+  connectionString,
+  max: 10,
+  idleTimeoutMillis: 5000
+});
+
+process.on('exit', function() {
+  db.end()
+    .then(() => console.log(`Shutting down connection pool to ${connectionString}`));
+});
+
+function lookupQuery(queryId: string, pool: Pool): Promise<string> {
+  return Promise.resolve('query{LocationSelection_getLocations(locationIds:[1,730099]){ locationId isBroadGeo}}');
+}
 
 const preregisteredQueryResolver: Plugin = {
   // TODO fill in preregistered query resolution. We basically
@@ -16,11 +42,16 @@ const preregisteredQueryResolver: Plugin = {
     let context = params['context'];
     let extensions = context['params' as keyof typeof context]['extensions'];
     console.log(extensions);
-    
+
     const maybePreregisteredId: string | null = extensions && extensions['preRegisteredQueryId'];
     if (maybePreregisteredId) {
       console.log("Got preregistered query id: " + maybePreregisteredId);
+      params.setParsedDocument(lookupQuery(maybePreregisteredId, db).then(params.parseFn));
     }
+  },
+  onContextBuilding(params) {
+    console.log(inspect({ onContextBuilding: params}));
+    console.log(params['context']);
   },
 };
 
