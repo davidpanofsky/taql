88a04a46581a5dfebf3e7d80393a7afc15f3b4c0
Scott Mitchell <smitchell@tripadvisor.com>
2023-03-29 10:55:10 -0400

    linting results

--- packages/schema/src/RemoveDirective.ts
+++ packages/schema/src/RemoveDirective.ts
@@ -1,8 +1,9 @@
-import { visit, Kind, SelectionSetNode, FragmentDefinitionNode, GraphQLError } from 'graphql';
-import { ExecutionRequest, ExecutionResult, relocatedError } from '@graphql-tools/utils';
-import { Transform, DelegationContext } from '@graphql-tools/delegate';
+import { DelegationContext, Transform } from '@graphql-tools/delegate';
+import { Kind, visit } from 'graphql';
+import { ExecutionRequest } from '@graphql-tools/utils';
 
-interface RemoveDirectiveTransformationContext extends Record<string, any> {}
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+type RemoveDirectiveTransformationContext = Record<string, any>;
 
 /**
  * Transform which removes a given directive from queries to modified subschemas.
@@ -10,26 +11,32 @@ interface RemoveDirectiveTransformationContext extends Record<string, any> {}
  * to the federation level.
  * https://the-guild.dev/graphql/stitching/docs/transforms
  */
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export default class RemoveDirective<TContext = Record<string, any>>
   implements Transform<RemoveDirectiveTransformationContext, TContext>
 {
   private readonly directive: string;
-  private readonly matcher: (directiveObj: { kind: string, name: { value: string } }) => boolean;
+  private readonly matcher: (directiveObj: {
+    kind: string;
+    name: { value: string };
+  }) => boolean;
   constructor(directive: string) {
     this.matcher = (directiveObj) => directiveObj?.name?.value === directive;
   }
 
   public transformRequest(
     originalRequest: ExecutionRequest,
-    delegationContext: DelegationContext<TContext>,
-    transformationContext: RemoveDirectiveTransformationContext
+    delegationContext: DelegationContext<TContext>, // eslint-disable-line @typescript-eslint/no-unused-vars
+    transformationContext: RemoveDirectiveTransformationContext // eslint-disable-line @typescript-eslint/no-unused-vars
   ): ExecutionRequest {
     const document = visit(originalRequest.document, {
       [Kind.FIELD]: {
-        enter: node => {
+        enter: (node) => {
           // Not all nodes have a directives field.
           if ('directives' in node && node.directives?.some(this.matcher)) {
-            const directives = node.directives.filter((directive) => !this.matcher(directive));
+            const directives = node.directives.filter(
+              (directive) => !this.matcher(directive)
+            );
             return {
               ...node,
               directives,
@@ -39,10 +46,10 @@ export default class RemoveDirective<TContext = Record<string, any>>
         },
       },
     });
-    
+
     return {
       ...originalRequest,
       document,
     };
-  } 
+  }
 }
