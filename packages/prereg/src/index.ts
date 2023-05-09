d58e4d0106d9c766e6439fb9780596b1b368ada4
Scott Mitchell <smitchell@tripadvisor.com>
2023-05-09 11:08:31 -0400

    remove default options object in usePreregisteredQueries definition

--- packages/prereg/src/index.ts
+++ packages/prereg/src/index.ts
@@ -98,7 +98,7 @@ export function usePreregisteredQueries(
     postgresConnectionString?: string;
     maxPoolSize?: number;
     poolConnectionTimeoutMillis?: number;
-  } = {}
+  }
 ): YogaPlugin {
   const {
     maxCacheSize,
