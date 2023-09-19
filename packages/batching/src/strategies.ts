bb7a616ce96c72d167e7f536a14891935d6d0349
David Panofsky <dpanofsky@tripadvisor.com>
2023-09-19 21:58:22 +0000

    Replace tracing with registerInstrumentations adding koa and http

--- packages/batching/src/strategies.ts
+++ packages/batching/src/strategies.ts
@@ -146,12 +146,12 @@ const insecureAllowedHeaders: Set<ForwardHeaderName> = new Set([
   'x-service-overrides',
 
   // tracing headers
-  'x-b3-traceid',
-  'x-b3-spanid',
-  'x-b3-parentspanid',
-  'x-b3-flags',
-  'x-b3-sampled',
-  'b3',
+  //'x-b3-traceid',
+  //'x-b3-spanid',
+  //'x-b3-parentspanid',
+  //'x-b3-flags',
+  //'x-b3-sampled',
+  //'b3',
 ]);
 
 const mergeInsecureHeaders = (requests: TaqlRequest[]) => {
