f109659b5136dd61d1b33c490eae7928125b0422
Josip Dujić <jdujic@tripadvisor.com>
2023-12-13 19:46:53 -0500

    Proxy GSR's get supergraph endpoint

--- packages/server/src/useGSRProxy.ts
+++ packages/server/src/useGSRProxy.ts
@@ -1,6 +1,7 @@
 import { SCHEMA, logger } from '@taql/config';
 import type { ComposeSubgraphsRequest } from '@gsr/api.endpoints.compose-subgraphs';
 import { IncomingMessage } from 'http';
+import { ParameterizedContext } from 'koa';
 import { TaqlMiddleware } from '@taql/context';
 import { authManager } from '@taql/executors';
 import { makeClient } from '@gsr/client';
@@ -24,25 +25,50 @@ function parseBody(req: IncomingMessage) {
   });
 }
 
-export function createComposeEndpoint(): TaqlMiddleware {
+export function createGSRProxy(): TaqlMiddleware {
   const manager = authManager(SCHEMA.oidcLiteAuthorizationDomain);
   const gsrClient = makeClient(SCHEMA, manager);
 
-  return async function useCompose(ctx, next) {
+  async function handleGSRReponse(
+    ctx: ParameterizedContext,
+    gsrPromise: ReturnType<(typeof gsrClient)[keyof typeof gsrClient]>
+  ) {
+    try {
+      const result = await gsrPromise;
+      ctx.status = parseInt(result.statusCode);
+      if ('body' in result) {
+        ctx.body = JSON.stringify(result.body);
+      }
+    } catch (err) {
+      logger.error(err);
+      ctx.status = 500;
+      ctx.body = err?.toString();
+    }
+  }
+
+  return async function useGSRProxy(ctx, next) {
     if (ctx.request.method === 'POST' && ctx.request.path === '/compose') {
-      try {
-        const result = await gsrClient.compose({
+      await handleGSRReponse(
+        ctx,
+        gsrClient.compose({
           body: await parseBody(ctx.req),
           method: 'POST',
           query: { environment: SCHEMA.environment },
-        });
-        ctx.status = parseInt(result.statusCode);
-        ctx.body = JSON.stringify(result.body);
-      } catch (err) {
-        logger.error(err);
-        ctx.status = 500;
-        ctx.body = err?.toString();
-      }
+        })
+      );
+    } else if (
+      ctx.request.method === 'GET' &&
+      ctx.request.path === '/supergraph'
+    ) {
+      await handleGSRReponse(
+        ctx,
+        gsrClient.supergraph({
+          method: 'GET',
+          query: {
+            environment: SCHEMA.environment,
+          },
+        })
+      );
     } else {
       await next();
     }
