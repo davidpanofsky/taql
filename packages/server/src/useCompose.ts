dde14b0081528e6be5fe0ad8fd3019b118c5e3d0
Josip Dujić <jdujic@tripadvisor.com>
2024-01-08 07:40:31 -0500

    Revert "Proxy GSR's get supergraph endpoint"
    This reverts commit f109659b5136dd61d1b33c490eae7928125b0422.

--- packages/server/src/useCompose.ts
+++ packages/server/src/useCompose.ts
@@ -1,7 +1,6 @@
 import { SCHEMA, logger } from '@taql/config';
 import type { ComposeSubgraphsRequest } from '@gsr/api.endpoints.compose-subgraphs';
 import { IncomingMessage } from 'http';
-import { ParameterizedContext } from 'koa';
 import { TaqlMiddleware } from '@taql/context';
 import { authManager } from '@taql/executors';
 import { makeClient } from '@gsr/client';
@@ -25,50 +24,25 @@ function parseBody(req: IncomingMessage) {
   });
 }
 
-export function createGSRProxy(): TaqlMiddleware {
+export function createComposeEndpoint(): TaqlMiddleware {
   const manager = authManager(SCHEMA.oidcLiteAuthorizationDomain);
   const gsrClient = makeClient(SCHEMA, manager);
 
-  async function handleGSRReponse(
-    ctx: ParameterizedContext,
-    gsrPromise: ReturnType<(typeof gsrClient)[keyof typeof gsrClient]>
-  ) {
-    try {
-      const result = await gsrPromise;
-      ctx.status = parseInt(result.statusCode);
-      if ('body' in result) {
-        ctx.body = result.body;
-      }
-    } catch (err) {
-      logger.error(err);
-      ctx.status = 500;
-      ctx.body = err?.toString();
-    }
-  }
-
-  return async function useGSRProxy(ctx, next) {
+  return async function useCompose(ctx, next) {
     if (ctx.request.method === 'POST' && ctx.request.path === '/compose') {
-      await handleGSRReponse(
-        ctx,
-        gsrClient.compose({
+      try {
+        const result = await gsrClient.compose({
           body: await parseBody(ctx.req),
           method: 'POST',
           query: { environment: SCHEMA.environment },
-        })
-      );
-    } else if (
-      ctx.request.method === 'GET' &&
-      ctx.request.path === '/supergraph'
-    ) {
-      await handleGSRReponse(
-        ctx,
-        gsrClient.supergraph({
-          method: 'GET',
-          query: {
-            environment: SCHEMA.environment,
-          },
-        })
-      );
+        });
+        ctx.status = parseInt(result.statusCode);
+        ctx.body = result.body;
+      } catch (err) {
+        logger.error(err);
+        ctx.status = 500;
+        ctx.body = err?.toString();
+      }
     } else {
       await next();
     }
